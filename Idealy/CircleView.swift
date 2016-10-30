fd398232f33f223295b54d3e0dd08aeec17f9750
Steven Graf <grafsteven@gmail.com>
2016-10-30 14:29:22 -0400

    Feed UI Part 1 complete

--- Idealy/CircleView.swift
+++ Idealy/CircleView.swift
@@ -1,25 +1,27 @@
 //
-//  EnhancedViewCustom.swift
+//  CircleView.swift
 //  Idealy
 //
-//  Created by Steven Graf on 10/24/16.
+//  Created by Steven Graf on 10/30/16.
 //  Copyright © 2016 Steven Graf. All rights reserved.
 //
 
 import UIKit
 
-class EnhancedViewCustom: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: Shadow_Gray, green: Shadow_Gray, blue: Shadow_Gray, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
+
     }
     
-
+      override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
 }
